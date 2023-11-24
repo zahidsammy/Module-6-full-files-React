16b75b89cd817ce86d636a6d467439e71b8a2d99
zahidsammy <120346801+zahidsammy@users.noreply.github.com>
2023-11-23 18:48:58 -0800

    Add files via upload

---
+++ Lab 6/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calculator from './component/Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Calculator App</h1>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
